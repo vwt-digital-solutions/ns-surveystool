60fa67c3b866c9a48d494a9878bd21620f833053
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-07-23 15:51:55 +0200

    Replace typeahead with dropdown. DAT-400

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgbAlertModule, NgbCollapseModule, NgbProgressbarModule, NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
-import {AgGridModule} from 'ag-grid-angular';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbAlertModule, NgbCollapseModule, NgbProgressbarModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxSelectModule, INgxSelectOptions } from 'ngx-select-ex';
 import { RegistrationDownloadButtonComponent } from './components/utils/registration-download-button.component';
 import { SearchComponent } from './components/search/search.component';
 import { DownloadButtonComponent } from './components/download-button/download-button.component';
+import { SelectComponent } from './components/select/select.component';
+import { SurveysService } from './services/surveys.service';
+
+const customSelectOptions: INgxSelectOptions = { // Check the interface for more options
+  keepSelectedItems: true
+};
 
 
 
@@ -22,19 +29,23 @@ import { DownloadButtonComponent } from './components/download-button/download-b
     HttpClientModule,
     AgGridModule.withComponents([RegistrationDownloadButtonComponent]),
     NgbAlertModule,
-    NgbProgressbarModule
+    NgbProgressbarModule,
+    NgxSelectModule.forRoot(customSelectOptions)
   ],
   declarations: [
     HomeComponent,
     RegistrationDownloadButtonComponent,
     SearchComponent,
     DownloadButtonComponent,
+    SelectComponent,
   ],
-  exports : [
+  exports: [
     CommonModule,
     HomeComponent
   ],
-  providers: [],
+  providers: [
+    SurveysService
+  ],
   bootstrap: [HomeComponent]
 })
 export class HomeModule { }
